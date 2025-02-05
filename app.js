91bb40f3122ca1f501f9cf1fafb46ff46ac341d8
tanisha-1909 <tanishamahavar32@gmail.com>
2025-02-05 23:52:59 +0530

    template literals,backtick and unicodes

---
+++ app.js
@@ -0,0 +1,45 @@
+// syntax of declaring variables
+
+let age=23;
+age=age+1
+let b;
+b=4;
+// power
+let side=3;
+side=side**2;
+
+// old syntax
+var x=10;
+var y=20;
+x+y;
+
+// assignment operator
+x=x+3;
+x;
+x/=2;
+x;
+
+// unary
+y--;
+
+let $=3;
+
+let namee="tony";
+namee[0];
+"tony"[0];
+
+namee.length;
+
+console.log("hello world");
+
+// template literal
+
+// backtick
+let pencil=5;
+let eraser=10;
+
+console.log(`total price is ${pencil+eraser} ruppes`);
+console.log(pencil<eraser);
+
+// unicodes
+console.log('A'<'a');
