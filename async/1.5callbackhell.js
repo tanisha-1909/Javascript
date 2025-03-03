80fe4aaab255ab7c25d309c80b87bb1adb68e0ba
tanisha-1909 <tanishamahavar32@gmail.com>
2025-03-03 15:11:06 +0530

    refactoring old code

--- async/1.5callbackhell.js
+++ async/1.5callbackhell.js
@@ -1,5 +1,5 @@
 let h1=document.querySelector("h1");
-
+let h2=document.querySelector("h2");
 // this way is inefficient
 setTimeout(() => {
     h1.style.color="red";
@@ -37,3 +37,34 @@ changeColor("red",1000,()=>{
 // changeColor("orange",2000);
 // changeColor("green",3000);
 
+
+// applying
+
+function changecolor(color,delay){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            h2.style.color=color;
+            resolve("color changed");
+        }, delay);
+    });
+};
+
+console.log(changecolor("red",10000));
+// Promise {<pending>}[[Prototype]]: Promise[[PromiseState]]: "pending"[[PromiseResult]]: undefined
+// Promise[[Prototype]]: Promise[[PromiseState]]: "fulfilled"[[PromiseResult]]: "color changed"
+
+changecolor("red",1000)
+.then(()=>{
+    console.log("red color was changed");
+    return changecolor("green",1000);
+})
+.then(()=>{
+    console.log("green color was changed");
+    return changecolor("blue",1000);
+})
+.then(()=>{
+    console.log("orange color was changed");
+})
+.catch(()=>{
+    console.log("error in changing color");
+});
